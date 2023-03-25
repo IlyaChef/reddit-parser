43e91a2fc09ddc2e1502bc65f22d96b0e9d92100
IlyaChef <ilyaromanov89@gmail.com>
2023-03-25 21:09:34 +0300

    api client test deleted due to GA features

--- tests/test_reddit_api_client.py
+++
@@ -1,7 +0,0 @@
-import praw
-from reddit_api_client import create_reddit_api_client
-
-
-def test_reddit_api_client_creation():
-    client = create_reddit_api_client()
-    assert isinstance(client, praw.Reddit)
